be1975606b74672248b290f45c5eae8422a1b637
Natalia <nataliaandreica@yahoo.com>
2020-02-16 12:08:01 +0200

    Updated

--- src/test/java/org/fastrackit/pages/CheckoutPage.java
+++ src/test/java/org/fastrackit/pages/CheckoutPage.java
@@ -8,56 +8,49 @@ public class CheckoutPage extends PageObject {
 
     @FindBy(id = "billing_first_name")
     private WebElementFacade firstNameField;
+    @FindBy(id = "billing_last_name")
+    private WebElementFacade lastNameField;
+    @FindBy(css = "#billing_address_1.input-text")
+    private WebElementFacade streetAddressField;
+    @FindBy(id = "billing_city")
+    private WebElementFacade cityField;
+    @FindBy(css = "#billing_postcode.input-text")
+    private WebElementFacade postcodeField;
+    @FindBy(id = "billing_phone")
+    private WebElementFacade phoneNumberField;
+    @FindBy(id = "place_order")
+    private WebElementFacade placeOrderButton;
+    @FindBy(css = ".woocommerce-notice")
+    private WebElementFacade confirmationMessage;
+
 
     public void setFirstName(String firstName) {
         typeInto(firstNameField, firstName);
     }
 
-    @FindBy(id = "billing_last_name")
-    private WebElementFacade lastNameField;
-
     public void setLastName(String lastName) {
         typeInto(lastNameField, lastName);
     }
-
-
-    @FindBy(css = "#billing_address_1.input-text")
-    private WebElementFacade streetAddressField;
 
     public void setStreetAddress(String streetAddress) {
         typeInto(streetAddressField, streetAddress);
     }
 
-    @FindBy(id = "billing_city")
-    private WebElementFacade cityField;
-
     public void setCityField(String city) {
         typeInto(cityField, city);
     }
-
-    @FindBy(css = "#billing_postcode.input-text")
-    private WebElementFacade postcodeField;
 
     public void setPostcodeField(String postcode) {
         typeInto(postcodeField, postcode);
     }
 
-    @FindBy(id = "billing_phone")
-    private WebElementFacade phoneNumberField;
-
     public void setPhoneNumberField(String phoneNumber) {
         typeInto(phoneNumberField, phoneNumber);
     }
 
-    @FindBy(id = "place_order")
-    private WebElementFacade placeOrderButton;
-
     public void clickOnPlaceOrderButton() {
         clickOn(placeOrderButton);
     }
-
-    @FindBy(css = ".woocommerce-notice")
-    private WebElementFacade confirmationMessage;
 
     private String text = "Thank you. Your order has been received.";
 
